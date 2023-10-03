ea09f713a9ae7619fd86e23c8586a72282ab0611
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-10-03 17:53:17 +0530

    server was listening successfully

---
+++ server/Server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const { default: mongoose } = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+const {MONGO_URI, PORT} = process.env;
+
+app.use(express.json({extended: false}));
+app.use(cors({origin: true, Credential: true}));
+
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => console.log(`Database connected successfully\nServer was listening in http://localhost:${PORT}`))
+    }).catch((e) => console.log(e.message));
+
+app.use((req, res) => {
+    res.status(200).json({message: "Run successfully"});    
+})
