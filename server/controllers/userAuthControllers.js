ebead682db0663b5b22b4e42aa06809cf41ebeaa
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-10-03 22:32:37 +0530

    user register server side was completed

--- server/controllers/userAuthControllers.js
+++ server/controllers/userAuthControllers.js
@@ -1,10 +1,18 @@
-const { default: mongoose } = require("mongoose");
+const bcrypt = require('bcryptjs');
 const userAuthModels = require("../models/userAuthModels");
 
 const addUserDetails = async (req, res) => {
     try {
-        const task = await userAuthModels.create(req.body);
-        res.status(200).json({message: "user registered successfully"});
+        const { uname, gmail } = req.body;
+        const preUser = await userAuthModels.findOne({gmail});
+        if (preUser) {
+            res.status(200).json({message: "User Already Exist"});
+            return;
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(req.body.pwd, salt);
+        const task = await userAuthModels.create({uname, gmail, pwd: hashPassword});
+        res.status(200).json({message: "user registered successfully", data: task});
     } catch (e) {
         res.status(400).json({message: e.message});
     }
