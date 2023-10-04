ca595abaffaeeac74668ed0c1997638b6fa9a258
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-10-04 22:18:20 +0530

    Navbar add from tailwindui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
 import React, { Fragment } from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <Fragment>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Navbar />
     </Fragment>
   )
 }
