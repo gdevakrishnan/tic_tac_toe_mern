0d39f235a05e4112a31f1cc65ef24ce567553a08
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-10-17 21:21:39 +0530

    navbar router auth components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
     <Fragment>
-      <h1>Tic Tac Toe Mern</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route index path='/' element={<Dashboard />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Logout' element={<Logout />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+        <Outlet />
+      </Router>
     </Fragment>
   )
 }
