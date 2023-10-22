634551fe3e1a5c9fd32dceb57ae11475b553864e
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-10-22 11:16:10 +0530

    Dashboard touch

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -7,7 +7,7 @@ function Dashboard() {
 
   return (
     <Fragment>
-      <section className="bg-gray-900 text-white h-screen">
+      <section className="bg-gray-900 text-white h-screen flex flex-col justify-center items-center">
         <div
           className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
         >
