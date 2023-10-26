c76b8525ae5642ec7bc51bccdfd267945f0a706e
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-10-26 10:22:45 +0530

    deploy

--- client/src/services/serviceWorker.js
+++ client/src/services/serviceWorker.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-const BASE_URL = "https://tic-tac-toe-mern-server.vercel.app";
+const BASE_URL = "http://localhost:5000/tic_tac_toe";
 
 export const addUserDetails = async (userDetails) => {
     const task = await Axios.post(`${BASE_URL}/register`, userDetails);
